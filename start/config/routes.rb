f534c2283f59286d323ad7167d47a3c1e10985bf
Ikeda.Y <aaiukrw@gmail.com>
2017-11-05 02:16:07 +0900

    api?

--- start/config/routes.rb
+++ start/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root to: "home#top" #startpage
   get 'posts/index'
 
-  get '/' => "home#top" #startpage
+  #get '/' => "home#top" #startpage
   get 'about' => "home#about"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
