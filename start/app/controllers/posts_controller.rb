e0b66ec6431c7ab12005957d1198072dd7eb1186
Ikeda.Y <aaiukrw@gmail.com>
2017-11-07 13:30:55 +0900

    api parts

--- start/app/controllers/posts_controller.rb
+++ start/app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
+
+  def rendJson
+  	#render  Post.all
+  	#@posts = Post.all
+  	render json: Post.all[params[:id].to_i]
+  	#paramsは文字列
+  end
+
+  def submit
+  	puts("success!!!!!")
+  	puts(params["content"])
+  	Post.create(id:"4",content:params["content"],created_at:"unknown",updated_at:"unknown")
+  	#render :text => params
+  end
 end
--- start/config/routes.rb
+++ start/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   #get '/' => "home#top" #startpage
-  get 'about' => "home#about"
+  get 'about' => "home#about" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'posts/rendJson/:id' => "posts#rendJson"
+  #postで受け取る部分を作成
+  post 'posts/submit' => "posts#submit"
 end
